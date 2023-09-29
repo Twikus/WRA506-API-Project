adee2f0e3c175a511f191715ae801390a188396d
Axel D <axel@spliiit.com>
2023-09-29 10:44:04 +0200

    feat: add routes

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,5 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MovieView from '../views/MovieView.vue'
+import MovieFileView from '../views/MovieFileView.vue'
+import ActorView from '../views/ActorView.vue'
+import ActorFileView from '../views/ActorFileView.vue'
+import CategoryView from '../views/CategoryView.vue'
+import CategoryFileView from '../views/CategoryFileView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +14,37 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
-    }
+    },
+    {
+      path: '/movie',
+      name: 'movie',
+      component: MovieView
+    },
+    {
+      path: '/movie/:id',
+      name: 'movie-file',
+      component: MovieFileView
+    },
+    {
+      path: '/actor',
+      name: 'actor',
+      component: ActorView
+    },
+    {
+      path: '/actor/:id',
+      name: 'actor-file',
+      component: ActorFileView
+    },
+    {
+      path: '/category',
+      name: 'category',
+      component: CategoryView
+    },
+    {
+      path: '/category/:id',
+      name: 'category-file',
+      component: CategoryFileView
+    },
   ]
 })
 
