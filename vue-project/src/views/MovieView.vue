63f947d311c9a2ded3acb6308ab41c6b37abed2b
Axel D <axel@spliiit.com>
2023-10-16 11:26:57 +0200

    feat: add search to movie

--- vue-project/src/views/MovieView.vue
+++ vue-project/src/views/MovieView.vue
@@ -1,23 +1,30 @@
-<script setup>
-import { onMounted, ref } from 'vue'
+<script setup lang="ts">
+import { onMounted, ref, computed } from 'vue'
 import axios from 'axios';
 import MovieCard from '../components/MovieCard.vue';
 
 const movies = ref([]);
+const search = ref('');
 
 onMounted(async () => {
     const response = await axios.get('https://localhost:8000/api/movies?page=1');
     movies.value = response.data['hydra:member'];
 })
-    ;
+
+const moviesFiltered = computed(() => {
+    return movies.value.filter((movie) => {
+        return movie.title.toLowerCase().includes(search.value.toLowerCase());
+    });
+})
 </script>
 
 <template>
     <div class="container-list-movies">
         <h1>Liste des Films</h1>
+        <input type="text" class="searchbar" v-model="search">
         <div class="container-movies">
             <div v-if="!movies">Chargement en cours...</div>
-            <MovieCard v-else v-for="movie in movies" :key="movie.id" :movie="movie" class="movie"/>
+            <MovieCard v-else v-for="movie in moviesFiltered" :key="movie.id" :movie="movie" class="movie"/>
         </div>
     </div>
 </template>
