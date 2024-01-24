93188a2e72f493d8d1c7c809b2c855d651629fc3
Axel D <axel@spliiit.com>
2024-01-24 16:54:14 +0100

    feat : add register

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
+import RegisterView from '../views/RegisterView.vue'
 import MovieView from '../views/Movie/MovieView.vue'
 import MovieFileView from '../views/Movie/MovieFileView.vue'
 import ActorView from '../views/Actor/ActorView.vue'
@@ -22,6 +23,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView
     },
     {
       path: '/movie',
